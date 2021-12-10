36e793f3b7c75eca47c32a56c196638c536b0c1c
EARTHES0412 <oheisagi@gmail.com>
2021-12-10 23:22:32 +0900

    Implements SearchItem page

---
+++ PSOBBCharacterDataDecoderWeb/Model/SearchResultModel.cs
@@ -0,0 +1,15 @@
+﻿using PSOBBCharactorGetter;
+
+namespace PSOBBCharacterDataDecoderWeb.Model
+{
+    /// <summary>
+    /// Psobb SearchResultModel
+    /// </summary>
+    public class SearchResultModel
+    {
+        public CharacterModel Character { get; set; }
+        public ItemModel Item { get; set; }
+
+        public string WhereIsIt { get; set; }
+    }
+}
---
+++ PSOBBCharacterDataDecoderWeb/Service/Interfaces/IPSOBBCharacterSearchService.cs
@@ -0,0 +1,17 @@
+﻿using PSOBBCharactorGetter;
+using PSOBBCharacterDataDecoderWeb.Model;
+
+namespace PSOBBCharacterDataDecoderWeb.Service.Interfaces
+{
+    /// <summary>
+    /// Interface of Decoding PSOBBCharacter Search Service
+    /// </summary>
+    public interface IPSOBBCharacterSearchService
+    {
+        /// <summary>
+        /// Search PSOBBCharacter's item
+        /// </summary>
+        /// <returns>IEnumerable of PSOBBCharactorGetter.Character having specify item</returns>
+        public Task<IEnumerable<SearchResultModel>> SearchItem(string item);
+    }
+}

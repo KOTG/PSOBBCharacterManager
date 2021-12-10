e96518affcd6b934f4aefd8fa60fe9fff9a3821d
EARTHES0412 <oheisagi@gmail.com>
2021-12-10 21:59:04 +0900

    Implements Decode Share Bank, and Divide logic from model

--- PSOBBCharacterDataDecoderWeb/Model/CharacterModel.cs
+++ PSOBBCharacterDataDecoderWeb/Model/CharacterModel.cs
@@ -3,58 +3,32 @@
 namespace PSOBBCharacterDataDecoderWeb.Model
 {
     /// <summary>
-    /// Psobb CharacterModel (charactor wrapper)
+    /// Psobb CharacterModel
     /// </summary>
     public class CharacterModel
     {
-        public Charactor charactor { get; set; }
-
         public IEnumerable<ItemModel> Items { get; set; }
 
         public IEnumerable<ItemModel> Banks { get; set; }
 
-        /// <summary>
-        /// Constructor
-        /// </summary>
-        /// <param name="hexCharactor"></param>
-        /// <param name="slotNumber"></param>
-        public CharacterModel(Charactor charactor)
-        {
-            this.charactor = charactor;
-
-            Items = charactor.Inventory.Select(x => new ItemModel()
-            {
-                ItemCode = x[0],
-                Item = x[1],
-                SlotNumber = x[2]
-            });
-
-            Banks = charactor.Bank.Select(x => new ItemModel()
-            {
-                ItemCode = x[0],
-                Item = x[1],
-                SlotNumber = x[2]
-            });
-        }
-
         public string Name
         {
-            get => charactor.Name;
+            get; set;
         }
 
         public string Level
         {
-            get => charactor.Lavel;
+            get; set;
         }
 
         public string Experience
         {
-            get => charactor.Experience;
+            get; set;
         }
 
         public string Race
         {
-            get => charactor.Race;
+            get; set;
         }
     }
 }
